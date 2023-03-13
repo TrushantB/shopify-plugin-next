5fcf0bc23a0851a617210bec7c73b5f02c8af425
arvindalawa07 <arvindalawa1998@gmail.com>
2023-03-13 15:17:18 +0530

    testing

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.post("/", async (req, res) => {
     }
   );
   const addedData = await response.json();
-  console.log("========>", addedData.variants[0]);
+  console.log("========>", addedData.product.variants[0]);
   // app.get("/", async (request, res) => {
   //   await fetch(
   //     "https://ekartbook.myshopify.com/admin/api/2023-01/products.json",
