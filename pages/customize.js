bcadd115c40958065ca21de1692dd9f1fd1633ac
arvindalawa07 <arvindalawa1998@gmail.com>
2023-04-10 19:05:19 +0530

    fixing build issue 1

--- pages/customize.js
+++ pages/customize.js
@@ -271,7 +271,7 @@ function Customize() {
       },
     };
 
-    console.log("your result is here", result);
+    console.log("your result is here1", result);
     try {
       await fetch("https://shopify-backend-x0gg.onrender.com", {
         method: "POST",
